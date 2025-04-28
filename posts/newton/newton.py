105be7e99063acbba49f75896a04bc2557d75a72
ellisterling <114019799+ellisterling@users.noreply.github.com>
2025-04-28 00:29:28 -0400

    started newton and adam

--- posts/newton/newton.py
+++ posts/newton/newton.py
@@ -2,7 +2,6 @@ import torch
 torch.manual_seed(1234)
 
 class LinearModel:
-
     def __init__(self, w=None):
         self.w = w
         
@@ -16,7 +15,6 @@ class LinearModel:
         return (scores >= 0).float()
     
 class LogisticRegression(LinearModel):
-
     def loss(self, X, y):
         scores = self.score(X)
         probs = self.sigmoid(scores)
@@ -26,6 +24,13 @@ class LogisticRegression(LinearModel):
     def sigmoid(self, z):
         return 1 / (1 + torch.exp(-z))
     
+    def hessian(self, X, y):
+        # replace  X with xik xij
+        scores = self.score(X)
+        probs = self.sigmoid(scores)
+        hessian = X * probs * (1 - probs)
+        return hessian
+
     def grad(self, X, y):
         scores = self.score(X)
         grad = 0
@@ -46,3 +51,27 @@ class GradientDescentOptimizer(LogisticRegression):
         temp_w = self.w.clone()
         self.w = self.w - alpha*gradient + beta*(self.w - self.prev_w)
         self.prev_w = temp_w
+
+class NewtonOptimizer(LogisticRegression):
+    def __init__(self):
+        LogisticRegression.__init__(self)
+        self.prev_w = self.w
+
+    def step(self, X, y, alpha):
+        gradient = self.grad(X, y)
+        hessian = self.hessian(X, y)
+        if self.prev_w is None:
+            self.prev_w = self.w.clone()
+        temp_w = self.w.clone()
+        self.w = self.w - alpha * torch.inverse(hessian) * gradient
+        self.prev_w = temp_w
+
+class AdamOptimizer(LogisticRegression):
+    def __init__(self):
+        LogisticRegression.__init__(self)
+
+    def step(self, X, y, batch_size, alpha, beta_1, beta_2, w_0=None):
+        if w_0 == None:
+            w_0 = torch.rand((X.size()[1]))
+            pass
+    
